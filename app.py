a9b5be459639259f8b601eee2f39e82eda249755
Hari Pulugurta <charanp@gmail.com>
2018-04-03 00:45:28 -0400

    adding a record in mongodb

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from flask import Flask
 from pymongo import MongoClient
 
 app = Flask(__name__)
-#client = MongoClient('localhost', 27017)
+client = MongoClient('db', 27017)
+db = client.test
 
 @app.route('/')
 def hello():
-    return 'Hello World'        
+    return 'Hello World v2'        
+
+@app.route('/create_post',methods=['POST'])
+def create_post():
+    post={"author": "Mike",
+         "text": "My first blog post!",
+         "tags": ["mongodb", "python", "pymongo"]
+    }
+    db.posts.insert(post)
 
 if __name__ == "__main__":
-    app.run(host="localhost", debug=True)
+    app.run(host="0.0.0.0", debug=True)
