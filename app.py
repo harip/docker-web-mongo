a2db5ccf6e526e5cb7f16e6296617d318d4f8ba1
Hari Pulugurta <charanp@gmail.com>
2018-04-03 00:53:37 -0400

    insert record into mongodb

--- app.py
+++ app.py
@@ -7,15 +7,19 @@ db = client.test
 
 @app.route('/')
 def hello():
-    return 'Hello World v2'        
+    return 'Hello World v3'        
 
-@app.route('/create_post',methods=['POST'])
+@app.route('/create_post',methods=['GET','POST'])
 def create_post():
     post={"author": "Mike",
          "text": "My first blog post!",
          "tags": ["mongodb", "python", "pymongo"]
     }
     db.posts.insert(post)
+
+    # return all posts
+    posts=db.posts.count()
+    return str(posts)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
